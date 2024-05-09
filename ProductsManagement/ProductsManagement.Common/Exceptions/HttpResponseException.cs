ed542a529cc09cabcd42bb9c14c81be0fa9751f7
Harold <halacazarp@gmail.com>
2024-05-08 22:25:40 -0500

    deleted unused class

--- ProductsManagement/ProductsManagement.Common/Exceptions/HttpResponseException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProductsManagement.Common.Exceptions
-{
-    public class HttpResponseException : Exception
-    {
-        public int Status { get; set; }
-        public object Value { get; set; }
-    }
-}
